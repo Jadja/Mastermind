6b7a2dbf0e89cbaa99c2beef8f026b56b69961ce
Jadja Laptop <jadjadebeste@gmail.com>
2015-10-13 11:00:56 +0200

    bla

---
+++ src/prj_p1/PRJ_P1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prj_p1;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Jadja
+ */
+public class PRJ_P1 {
+    
+    
+    // Values
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFrame frame = new NewJFrame();
+        frame.setVisible(true);
+    }
+    
+}
